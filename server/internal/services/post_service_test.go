33532885410ae136156352f01200672af51c5367
Alex-Jr <50220189+Alex-Jr@users.noreply.github.com>
2023-11-23 22:04:36 -0300

    test: s - post delete

--- server/internal/services/post_service_test.go
+++ server/internal/services/post_service_test.go
@@ -78,6 +78,107 @@ func TestPostServiceCreate(t *testing.T) {
 	)
 }
 
+func TestPostServiceDelete(t *testing.T) {
+	postRepository := mocks.NewMockPostRepository(t)
+
+	postService := services.NewPostService(services.PostServiceConfig{
+		PostRepository: postRepository,
+	})
+
+	postRepository.On(
+		"GetFull",
+		context.TODO(),
+		1,
+	).Return(
+		fakes.LoadPostRelations(fakes.Post1),
+		nil,
+	)
+
+	postRepository.On(
+		"Delete",
+		context.TODO(),
+		1,
+	).Return(
+		nil,
+	)
+
+	response, err := postService.Delete(context.TODO(), dtos.DeletePostDTO{
+		ID: 1,
+	})
+	require.NoError(t, err)
+
+	b, err := json.Marshal(response)
+	require.NoError(t, err)
+
+	require.JSONEq(t, `
+		{
+			"post":		{
+				"created_at": "2020-01-01T00:00:00Z",
+				"description": "post 1 description",
+				"id": 1,
+				"pool_count": 4,
+				"pools": [
+					{
+						"created_at": "2022-01-01T00:00:00Z",
+						"custom": ["a"],
+						"description": "pool 1 description",
+						"id": 1,
+						"name": "pool 1 name",
+						"post_count": 1,
+						"posts": null,
+						"updated_at": "2022-01-01T00:00:00Z"
+					},
+					{
+						"created_at": "2022-01-01T00:00:00Z",
+						"custom": [],
+						"description": "pool 2 description",
+						"id": 2,
+						"name": "pool 2 name",
+						"post_count": 2,
+						"posts": null,
+						"updated_at": "2022-01-01T00:00:00Z"
+					},
+					{
+						"created_at": "2020-01-01T00:00:00Z",
+						"custom": [],
+						"description": "pool 3 description",
+						"id": 3,
+						"name": "pool 3 name",
+						"post_count": 3,
+						"posts": null,
+						"updated_at": "2020-01-01T00:00:00Z"
+					},
+					{
+						"created_at": "2020-01-01T00:00:00Z",
+						"custom": ["shared"],
+						"description": "pool 4 description",
+						"id": 4,
+						"name": "pool 4 name",
+						"post_count": 1,
+						"posts": null,
+						"updated_at": "2020-01-01T00:00:00Z"
+					}
+				],
+				"rating": "S",
+				"tag_count": 1,
+				"tag_ids": ["tag_one"],
+				"tags": [
+					{
+						"id": "tag_one",
+						"post_count": 1,
+						"created_at": "2020-01-01T00:00:00Z",
+						"updated_at": "2020-01-01T00:00:00Z",
+						"description": "tag one description"
+					}
+				],
+				"updated_at": "2020-01-01T00:00:00Z"
+			}
+		}
+		`,
+		string(b),
+	)
+}
+
 func TestPostServiceFetch(t *testing.T) {
 	postRepository := mocks.NewMockPostRepository(t)
 
