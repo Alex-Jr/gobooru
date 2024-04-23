5dd80b57ab72e63fefeb612e93addf6b64b6df1d
Alex-Jr <50220189+Alex-Jr@users.noreply.github.com>
2024-04-23 13:48:37 -0300

    feat(client): tag suggestions

--- client/src/pages/post-list/components/sidebar.tsx
+++ client/src/pages/post-list/components/sidebar.tsx
@@ -1,13 +1,11 @@
 import { SearchIcon } from "@chakra-ui/icons";
-import {
-  Flex,
-  FormControl,
-  FormLabel,
-  IconButton,
-  Input,
-} from "@chakra-ui/react";
-import { useForm } from "react-hook-form";
+import { Flex, FormControl, FormLabel, IconButton } from "@chakra-ui/react";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
+
+import { useTagsLists } from "services/tags/use-tags-list";
+import { InputWithSuggestions } from "shared/components/forms/input-with-suggestions";
 
 interface IFormData {
   search: string;
@@ -24,6 +22,18 @@ export const Sidebar = ({ defaultValues, onSubmit }: ISidebarProps) => {
     defaultValues,
   });
 
+  const [search, setSearch] = useState(defaultValues.search);
+
+  const { tags } = useTagsLists({
+    page: "1",
+    page_size: "10",
+    search: search,
+  });
+
+  const tagSuggestions = tags.map((tag) => ({
+    value: tag.id,
+  }));
+
   return (
     <Flex
       as={"form"}
@@ -39,7 +49,22 @@ export const Sidebar = ({ defaultValues, onSubmit }: ISidebarProps) => {
         <FormLabel htmlFor="search">{t("glossary.search")}</FormLabel>
 
         <Flex gap={2} mt={2}>
-          <Input {...register("search")} size={"sm"} />
+          <Controller
+            name="search"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <InputWithSuggestions
+                {...field}
+                suggestions={tagSuggestions}
+                onTypeEnd={(value) => {
+                  const words = value.split(" ");
+
+                  setSearch(words[words.length - 1]);
+                }}
+              />
+            )}
+          />
 
           <IconButton aria-label="search" type="submit" size={"sm"}>
             <SearchIcon />
--- client/src/shared/components/forms/input-with-suggestions.tsx
+++ client/src/shared/components/forms/input-with-suggestions.tsx
@@ -1,72 +1,136 @@
-import { Input } from "@chakra-ui/react";
-import React, { KeyboardEvent, forwardRef, useRef } from "react";
+import {
+  Input,
+  List,
+  ListItem,
+  Popover,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
+} from "@chakra-ui/react";
+import React, { forwardRef, useRef, useState } from "react";
+import { ControllerRenderProps } from "react-hook-form";
 
-interface InputWithSuggestionsProps {
-  // value: string;
-  name: string;
-  // onChange: (...event: any[]) => void;
-  disabled?: boolean;
-  onBlur: any;
-  suggestions: { value: string; text: string }[];
+import { useDebounce } from "shared/hooks/use-debounce";
+
+interface InputWithSuggestionsProps extends ControllerRenderProps {
+  suggestions: { value: string; text?: string }[];
   onTypeEnd: (value: string) => void;
-  onKeyDown?: (e: KeyboardEvent<HTMLInputElement>) => void;
 }
 
 export const InputWithSuggestions = forwardRef<
   HTMLInputElement,
   InputWithSuggestionsProps
 >(function InputWithSuggestions(props, ref) {
-  const innerRef = useRef<HTMLInputElement | null>();
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
-  let timeout: NodeJS.Timeout | null = null;
+  const [highlightedIndex, setHighlightedIndex] = useState(-1);
+
+  const onTypeEndDebounced = useDebounce(props.onTypeEnd, 500);
+
+  const suggestions = props.suggestions.filter((suggestion) => {
+    return !props.value.includes(suggestion.value);
+  });
 
   return (
     <>
-      <Input
-        ref={(element) => {
-          innerRef.current = element;
+      <Popover initialFocusRef={inputRef}>
+        {({ isOpen, onClose }) => (
+          <>
+            <PopoverTrigger>
+              <Input
+                name={props.name}
+                autoComplete="off"
+                size={"sm"}
+                ref={(el) => {
+                  inputRef.current = el;
+                  if (ref) {
+                    if (typeof ref === "function") {
+                      ref(inputRef.current);
+                    }
+                  }
+                }}
+                value={props.value}
+                onChange={(e) => {
+                  props.onChange(e);
 
-          if (typeof ref === "function") {
-            ref(element);
-          } else {
-            innerRef.current = element;
-          }
-        }}
-        onBlur={props.onBlur}
-        name={props.name}
-        autoComplete="off"
-        position={"relative"}
-        list={`${props.name}suggestions`}
-        onKeyDown={(e) => {
-          // debounce
-          if (timeout) {
-            clearTimeout(timeout);
-          }
+                  onTypeEndDebounced(e.target.value);
+                }}
+                onKeyDown={(e) => {
+                  // console.log("key down", e.key);
+                  if (e.key === "ArrowDown") {
+                    setHighlightedIndex((prevIndex) =>
+                      Math.min(prevIndex + 1, suggestions.length - 1)
+                    );
+                    e.preventDefault();
+                  } else if (e.key === "ArrowUp") {
+                    setHighlightedIndex((prevIndex) =>
+                      Math.max(prevIndex - 1, -1)
+                    );
+                    e.preventDefault();
+                  } else if (e.key === "Enter") {
+                    if (highlightedIndex === -1) {
+                      onClose();
+                      return;
+                    }
 
-          timeout = setTimeout(() => {
-            if (innerRef.current?.value)
-              props.onTypeEnd(innerRef.current?.value);
-          }, 1000);
+                    const words = props.value.split(" ");
 
-          // prevent spamming when Enter or Space triggers an rerender
-          if (e.key === "Enter" || e.key === " ") {
-            clearTimeout(timeout);
-          }
+                    console.log(
+                      words
+                        .slice(0, words.length - 1)
+                        .concat(suggestions[highlightedIndex].value)
+                        .join(" ")
+                        .trim()
+                    );
 
-          if (props.onKeyDown) props.onKeyDown(e);
-        }}
-      />
+                    props.onChange(
+                      words
+                        .filter((w: string) => w !== "")
+                        .slice(
+                          0,
+                          words[words.length - 1] === ""
+                            ? words.length
+                            : words.length - 1
+                        )
+                        .concat(suggestions[highlightedIndex].value)
+                        .join(" ")
+                    );
 
-      <datalist id={`${props.name}suggestions`}>
-        {props.suggestions.map((s) => (
-          <option
-            key={`${props.name}-suggestion-${s.text}-${s.value}`}
-            value={s.value}
-          >
-            {s.text}
-          </option>
-        ))}
-      </datalist>
+                    setHighlightedIndex(-1);
+
+                    e.preventDefault();
+                  }
+                }}
+                onBlur={(e) => {
+                  onClose();
+                }}
+              />
+            </PopoverTrigger>
+            {suggestions.length > 0 && (
+              <PopoverContent justifySelf={"start"}>
+                <PopoverBody p={0}>
+                  <List>
+                    {suggestions.map((suggestion, index) => (
+                      <ListItem
+                        fontSize={"sm"}
+                        key={index}
+                        bg={
+                          highlightedIndex === index
+                            ? "gray.800"
+                            : "transparent"
+                        }
+                        p={2}
+                      >
+                        {suggestion.value}
+                      </ListItem>
+                    ))}
+                  </List>
+                </PopoverBody>
+              </PopoverContent>
+            )}
+          </>
+        )}
+      </Popover>
     </>
   );
 });
